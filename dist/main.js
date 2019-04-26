95a41966bc6dc1a0654f87c1cc590240a9d2ee1b
Luke Harrison <luke@lukeharrison.net>
2019-04-26 18:22:09 +0100

    WIP

--- dist/main.js
+++ dist/main.js
@@ -4,9 +4,9 @@
 
 // Logic to get the components functioning on a basic level. Nothing too elaborate.
 
-const expanders = document.querySelectorAll('.o-expander');
+const expanders = document.querySelectorAll('.c-expander');
 for(let i = 0; i < expanders.length; i++) {
-	const triggers = expanders[i].querySelectorAll('.o-expander__trigger');
+	const triggers = expanders[i].querySelectorAll('.c-expander__trigger');
 	for(let t = 0; t < triggers.length; t++) {
 		triggers[t].addEventListener('click', () => {
 			expanders[i].classList.toggle('is-active');
